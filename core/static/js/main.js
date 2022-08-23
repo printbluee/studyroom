4b40b54107926cf614a661a724e1caa7e43b4d98
진혜정 <yhdosa0622@gmail.com>
2022-08-23 21:56:14 +0900

    메인페이지

---
+++ core/static/js/main.js
@@ -0,0 +1,41 @@
+const settingIcon = document.querySelectorAll(".setting_icon");
+const settingModal = document.querySelectorAll(".setting_icon-modal");
+const roomModify = document.querySelectorAll(".room_modify");
+const roomRemove = document.querySelectorAll(".room_remove");
+
+for (let i = 0; i < settingIcon.length; i++) {
+
+    settingIcon[i].addEventListener("click", function(){
+        settingModal[i].classList.toggle("ds_block");
+    })
+}
+
+// 세팅 아이콘 클릭시 수정,삭제 버튼 토글
+
+for (let i = 0; i < settingIcon.length; i++) {
+    roomRemove[i].addEventListener("click", function(){
+       alert("삭제하시겠습니까?")
+    })
+
+}
+
+// 삭제버튼 클릭시 한번더 확인
+
+const mainCon = document.querySelectorAll(".main_con")
+const onerProfile = document.querySelectorAll(".oner_box-profile");
+const onerSecret = document.querySelectorAll(".oner_box-secret");
+
+for (let i = 0; i < mainCon.length; i++) {
+
+    if(mainCon[i].classList.contains("secret") ) {
+        // console.log(i+ "번째있어")
+        onerProfile[i].className += " ds-none";
+        
+    }else {
+        onerSecret[i].className += " ds-none";
+    }
+    
+}
+
+// 비밀방일경우 방생성시 .main_con에 secret 클래스가 붙음
+// scret클래스가 있을시 열쇠 아이콘 생성
