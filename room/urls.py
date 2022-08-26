5d882961d1f8bcadcd16e04cdaa0ae382bcf85f9
진혜정 <yhdosa0622@gmail.com>
2022-08-26 16:53:50 +0900

    room 추가 페이지(글쓰기)

--- room/urls.py
+++ room/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     # path('search',views.search, name="search"), #검색
     # path('room/page/<int:pk>', views.room_page, name='room_page'), #글상세페이지
 
+    path('room/', views.room_add, name='room_add'), # 글추가
     # path('room/edit/<int:pk>',views.room_edit, name="room_edit"), #글수정
     # path('room/delete/<int:pk>',views.room_delete, name='room_delete'), #글삭제
-    # path('room/', views.room_add, name='room_add'), # 글추가
-
+    
     # # path('room/join/<int:pk>',views.room_join, name="room_join"), #가입
 ]
--- room/views.py
+++ room/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+#모델 가져오기
+from room.models import Room
+
+#글쓰기추가  
+@login_required
+def room_add(request):
+    if request.method == 'POST': # post요청일 경우 if문 실행
+        title = request.POST.get('title', None)
+        sub_title = request.POST.get('sub_title', None)
+        content = request.POST.get('content', None) 
+        room_password = request.POST.get('room_password', None) 
+        room_owner = request.user 
+
+        if not title:
+            return render(request, 'room_add.html', {"error" : "방제목를 입력 해주세요."}) 
+            
+        if not sub_title:
+            return render(request, 'room_add.html', {"error" : "부제목을 입력 해주세요"}) 
+
+        if not content:
+            return render(request, 'room_add.html', {"error" : "내용을 입력 해주세요"})
+
+        if room_password:
+            if len(room_password) != 5: # 비밀번호는 필수가 아니지만 있을 경우 숫자 5글자
+                return render(request, 'room_add.html', {"error" : "방 비밀번호는 숫자 5글자로 설정해주세요"})
+
+        if (title and sub_title and content):
+            PostingObj = Room()
+            PostingObj.title = title
+            PostingObj.sub_title = sub_title
+            PostingObj.content = content
+            PostingObj.room_owner = room_owner
+            PostingObj.room_password =room_password
+            PostingObj.save() # 저장
+            return redirect('main') # core/views.py에 main함수 이동(메인페이지)
+
+        else:
+            return render(request, 'room_add.html', {"error" : "에러발생 모든 내용을 입력 해주세요"}) 
+    else:
+        return render(request, 'room_add.html')
+
+
